a6fd64da12a97001461344c93259feb4e0d5c12d
gaaon <xodn4195@gmail.com>
2024-04-03 19:12:34 +0900

    feat: DecreaseByItemId 테스트 통과

--- practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepository.java
+++ practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepository.java
@@ -3,13 +3,16 @@ package com.grizz.inventoryapp.inventory.repository;
 import com.grizz.inventoryapp.inventory.repository.entity.InventoryEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface InventoryJpaRepository extends JpaRepository<InventoryEntity, Long> {
     @NotNull Optional<InventoryEntity> findByItemId(@NotNull String itemId);
 
-    @NotNull default Integer decreaseStock(@NotNull String itemId, @NotNull Long quantity) {
-        return -1;
-    }
+    @Modifying
+    @Query("update InventoryEntity i set i.stock = i.stock - :quantity " +
+            "where i.itemId = :itemId")
+    @NotNull Integer decreaseStock(@NotNull String itemId, @NotNull Long quantity);
 }
