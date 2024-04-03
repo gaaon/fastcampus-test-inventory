c6f62452420f7e47d40d55709b968fd9d8b4efc4
gaaon <xodn4195@gmail.com>
2024-04-03 19:49:28 +0900

    test: 재고 조회 실패 테스트 추가

--- practices/inventory/src/test/java/com/grizz/inventoryapp/integration/InventoryIntegrationTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/integration/InventoryIntegrationTest.java
@@ -1,14 +1,34 @@
 package com.grizz.inventoryapp.integration;
 
+import com.grizz.inventoryapp.inventory.controller.consts.ErrorCodes;
 import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class InventoryIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    final String nonExistingItemId = "2";
+
     @DisplayName("재고 조회 실패")
     @Test
-    void test1() {
-        throw new NotImplementedTestException();
+    void test1() throws Exception {
+        // given
+        mockMvc.perform(get("/api/v1/inventory/{itemId}", nonExistingItemId))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error.code").value(ErrorCodes.ITEM_NOT_FOUND.code))
+                .andExpect(jsonPath("$.error.local_message").value(ErrorCodes.ITEM_NOT_FOUND.message));
     }
 
     @DisplayName("재고 조회 성공")
