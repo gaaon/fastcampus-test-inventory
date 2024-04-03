bf025176b3e222a2efb6e6ee52985b6604e62c25
gaaon <xodn4195@gmail.com>
2024-04-03 19:47:15 +0900

    test: InventoryIntegrationTest 뼈대 추가

---
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/integration/InventoryIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.grizz.inventoryapp.integration;
+
+import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InventoryIntegrationTest {
+    @DisplayName("재고 조회 실패")
+    @Test
+    void test1() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 조회 성공")
+    @Test
+    void test2() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 차감 실패")
+    @Test
+    void test3() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 차감 성공")
+    @Test
+    void test4() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 수정 실패")
+    @Test
+    void test5() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 수정 성공")
+    @Test
+    void test6() {
+        throw new NotImplementedTestException();
+    }
+
+    @DisplayName("재고 차감, 수정 종합")
+    @Test
+    void test7() {
+        throw new NotImplementedTestException();
+    }
+}
