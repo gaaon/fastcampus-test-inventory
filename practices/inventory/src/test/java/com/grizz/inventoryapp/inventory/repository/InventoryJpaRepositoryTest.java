36c37f55db730edbccd3d61f52c943e4f835ac01
gaaon <xodn4195@gmail.com>
2024-04-03 19:15:31 +0900

    test: Save 테스트 추가

--- practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepositoryTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.grizz.inventoryapp.inventory.repository;
 
 import com.grizz.inventoryapp.inventory.repository.entity.InventoryEntity;
-import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles("h2-test")
@@ -86,16 +86,41 @@ public class InventoryJpaRepositoryTest {
 
     @Nested
     class Save {
+        final Long existingId = 1L;
+        final String existingItemId = "1";
+        final String nonExistingItemId = "2";
+
         @DisplayName("id를 갖는 entity가 없다면, entity를 추가하고 추가된 entity를 반환한다")
         @Test
         void test1() {
-            throw new NotImplementedTestException();
+            // given
+            final Long newStock = 1234L;
+
+            // when
+            final InventoryEntity entity = new InventoryEntity(null, nonExistingItemId, newStock);
+            final InventoryEntity result = sut.save(entity);
+
+            // then
+            assertNotNull(result.getId());
+            assertEquals(nonExistingItemId, result.getItemId());
+            assertEquals(newStock, result.getStock());
         }
 
         @DisplayName("id를 갖는 entity가 있다면, entity를 수정하고 수정된 entity를 반환한다")
         @Test
         void test2() {
-            throw new NotImplementedTestException();
+            // given
+            final Long newStock = 1234L;
+            final InventoryEntity entity = sut.findByItemId(existingItemId).get();
+
+            // when
+            entity.setStock(newStock);
+            final InventoryEntity result = sut.save(entity);
+
+            // then
+            assertEquals(1L, result.getId());
+            assertEquals(existingItemId, result.getItemId());
+            assertEquals(newStock, result.getStock());
         }
     }
 }
