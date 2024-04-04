731c1e8286382ccbafeba271974d50c69e78cd05
gaaon <xodn4195@gmail.com>
2024-04-04 11:18:45 +0900

    test: InventoryPersistenceAdapterImplTest DecreaseStock 테스트 추가

--- practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryPersistenceAdapterImplTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryPersistenceAdapterImplTest.java
@@ -61,16 +61,36 @@ public class InventoryPersistenceAdapterImplTest {
 
     @Nested
     class DecreaseByItemId {
+        final String existingItemId = "1";
+        final String nonExistingItemId = "2";
+        final Long stock = 100L;
+
+        @BeforeEach
+        void setUp() {
+            inventoryJpaRepositoryStub.addInventoryEntity(existingItemId, stock);
+        }
+
         @DisplayName("itemId를 갖는 entity가 없다면, null을 반환한다")
         @Test
         void test1() {
-            throw new NotImplementedTestException();
+            // when
+            final Inventory result = sut.decreaseStock(nonExistingItemId, 10L);
+
+            // then
+            assertNull(result);
         }
 
         @DisplayName("itemId를 갖는 entity가 있다면, stock을 차감하고 inventory를 반환한다")
         @Test
         void test2() {
-            throw new NotImplementedTestException();
+            // when
+            final Inventory result = sut.decreaseStock(existingItemId, 10L);
+
+            // then
+            assertNotNull(result);
+
+            final Long expectedStock = stock - 10L;
+            assertEquals(expectedStock, result.getStock());
         }
     }
 
