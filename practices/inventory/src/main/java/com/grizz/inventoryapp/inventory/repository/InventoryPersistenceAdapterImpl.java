1c78df14f734bd3e5d5099700f9bb078e7f2a7b3
gaaon <xodn4195@gmail.com>
2024-04-04 11:15:32 +0900

    test: InventoryPersistenceAdapterImplTest FindByItemId 테스트 추가

---
+++ practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/repository/InventoryPersistenceAdapterImpl.java
@@ -0,0 +1,30 @@
+package com.grizz.inventoryapp.inventory.repository;
+
+import com.grizz.inventoryapp.inventory.repository.jpa.InventoryJpaRepository;
+import com.grizz.inventoryapp.inventory.service.domain.Inventory;
+import com.grizz.inventoryapp.inventory.service.persistence.InventoryPersistenceAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class InventoryPersistenceAdapterImpl implements InventoryPersistenceAdapter {
+    private final InventoryJpaRepository inventoryJpaRepository;
+
+    public InventoryPersistenceAdapterImpl(InventoryJpaRepository inventoryJpaRepository) {
+        this.inventoryJpaRepository = inventoryJpaRepository;
+    }
+
+    @Override
+    public @Nullable Inventory findByItemId(@NotNull String itemId) {
+        return null;
+    }
+
+    @Override
+    public @Nullable Inventory decreaseStock(@NotNull String itemId, @NotNull Long quantity) {
+        return null;
+    }
+
+    @Override
+    public @NotNull Inventory save(@NotNull Inventory inventory) {
+        return null;
+    }
+}
--- practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryPersistenceAdapterImplTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryPersistenceAdapterImplTest.java
@@ -1,23 +1,61 @@
 package com.grizz.inventoryapp.inventory.repository;
 
+import com.grizz.inventoryapp.inventory.repository.jpa.InventoryJpaRepositoryStub;
+import com.grizz.inventoryapp.inventory.service.domain.Inventory;
 import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ExtendWith(MockitoExtension.class)
 public class InventoryPersistenceAdapterImplTest {
+    @InjectMocks
+    InventoryPersistenceAdapterImpl sut;
+
+    @Spy
+    InventoryJpaRepositoryStub inventoryJpaRepositoryStub;
+
     @Nested
     class FindByItemId {
+        final String existingItemId = "1";
+        final String nonExistingItemId = "2";
+        final Long stock = 100L;
+
+        @BeforeEach
+        void setUp() {
+            inventoryJpaRepositoryStub.addInventoryEntity(existingItemId, stock);
+        }
+
         @DisplayName("itemId를 갖는 entity가 없다면, null을 반환한다")
         @Test
         void test1() {
-            throw new NotImplementedTestException();
+            // when
+            final Inventory result = sut.findByItemId(nonExistingItemId);
+
+            // then
+            assertNull(result);
         }
 
         @DisplayName("itemId를 갖는 entity가 있다면, inventory를 반환한다")
         @Test
         void test2() {
-            throw new NotImplementedTestException();
+            // when
+            final Inventory result = sut.findByItemId(existingItemId);
+
+            // then
+            assertNotNull(result);
+
+            assertEquals(existingItemId, result.getItemId());
+            assertEquals(stock, result.getStock());
         }
     }
 
